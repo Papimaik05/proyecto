3cabd597a2b4a74393fe18a6cb1f50a888ffe5aa
camore12 <c14moreno1p02@gmail.com>
2023-02-27 12:38:27 +0100

    estetica tienda

--- tienda.php
+++ tienda.php
@@ -13,8 +13,10 @@
 <body>
     <h1>Tienda</h1>
 <section>
+    <img src="./img/pesca_inaki.jpg">
     <img src="./img/pinguinos_madagascar.jpg">
-    <img src="./img/pinguinos_madagascar.jpg">
+    <img src="./img/nemo.jpg">
+    <img src="./img/jaula_tiburones.jpg">
 </section>
 
 </body>
