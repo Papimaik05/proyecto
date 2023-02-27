688002c27abe73dcaee288531633249000cf40d1
Papimaik05 <miguelmateosmatias@gmail.com>
2023-02-27 12:07:23 +0100

    bk

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     </head>
 
 <body>
-    <div >
+    <div style="text-align:center;">
             <?php
             require ('./basico/cabecera.php');
             ?>
 	    <main>
 	        <article>
 		        <h1>AMIGOS MARINOS</h1>
-                <h2>Bajo el Mar,nada va mal</h2>                
+                <h2>Bajo el Mar, Nada va mal</h2>                
                 <br>
                 <img src="./img/logo.jpg"  width ="580"  height ="480"  alt="logotipo">
 	        </article>
