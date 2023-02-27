bc917514dddf2208087db462021988e994c79446
Papimaik05 <miguelmateosmatias@gmail.com>
2023-02-27 11:45:14 +0100

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Index</title>
+    </head>
+
+<body>
+    <?php   
+    require("includes/basico/cabecera.php");
+    ?>
+
+    <h1>Bienvenido a AMIGOS MARINOS</h1>
+    <h2>Bajo el Mar,nada va mal</h2>
+</body>
+</html>
