b4ff5f63bd60ae1f0a9b7c9efa4c19a017c52599
Papimaik05 <miguelmateosmatias@gmail.com>
2023-02-27 12:02:20 +0100

    sc

--- index.php
+++ index.php
@@ -8,11 +8,18 @@
     </head>
 
 <body>
-    <?php   
-    require("includes/basico/cabecera.php");
-    ?>
-
-    <h1>Bienvenido a AMIGOS MARINOS</h1>
-    <h2>Bajo el Mar,nada va mal</h2>
+    <div >
+            <?php
+            require ('./basico/cabecera.php');
+            ?>
+	    <main>
+	        <article>
+		        <h1>AMIGOS MARINOS</h1>
+                <h2>Bajo el Mar,nada va mal</h2>                
+                <br>
+                <img src="./img/logo.jpg"  width ="580"  height ="480"  alt="logotipo">
+	        </article>
+	    </main>         
+    </div>     
 </body>
 </html>
