21c8c811119fdd83ecffd0e5d4f2d315abf488e9
elias-jr7 <eliasjan@ucm.es>
2023-03-01 16:54:46 +0100

    login

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>
+    Login pagina web
+</title>
+<link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<?php
+
+
+?>
+<body>
+<h1>
+    Iniciar sesión
+</h1>
+<form action="procesarLogin.php" method="post">
+<p>Usuario</p>
+<input type="text" name="usuario"/>
+<p>Contraseña</p>
+<input type="password" name="contraseña"/>
+<p><input type="submit" value="Iniciar sesión" /></p>
+</form>
+
+</body>
+
+
+
+</html>
