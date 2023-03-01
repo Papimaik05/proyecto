f455e049b421c479b1bdd541b5183b29493923cc
Papimaik05 <miguelmateosmatias@gmail.com>
2023-03-01 11:33:58 +0100

    siu

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
 	        <article>  
                 <br>          
                 <img src="./img/logo.jpg"  width ="580"  height ="480"  alt="logotipo">
+                <br>
+                <button onclick="window.location.href='login.php'" type="button"  > Inicio de Sesión</button>
+		        <button onclick="window.location.href='registro.php'" type="button" > Registrate</button>
 	        </article>
 	    </main>         
     </div>     
