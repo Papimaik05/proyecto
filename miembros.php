8a6dc9d25351b0cc99f814c0f26169be6f651cd1
mmiralle17 <mmiralle@ucm.es>
2023-03-03 14:31:26 +0100

    pie de pagina

---
+++ miembros.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset= "utf-8">
+	<title>Miembros del equipo</title>
+	<link id="estilo" href="./css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<!-- Logo en pequeño con el titulo -->
+	<h1 id="principio">
+		<img src="./img/logo.jpg"
+			width="50"
+			alt = "logo"
+			height="50"
+		>
+
+	Amigos Marinos
+	</h1>
+	<!-- Enlace a las demas paginas -->
+	<h2>
+		Índice
+	</h2>
+	<menu>
+			<li><a href="index.html">Inicio</a></li>
+			<li><a href="detalles.html">Detalles</a></li>
+			<li><a href="bocetos.html">Bocetos</a></li>
+			<li><a href="miembros.html">Miembros</a></li>
+			<li><a href="planificacion.html">Planificación</a></li>
+			<li><a href="contacto.html">Contacto</a></li>
+	</menu>
+	<!-- Enlaces directos a los miembros -->
+	<h2>
+		Enlaces directos a los miembros
+	</h2>
+	<a href = "#Maria">1.María Magdalena Miralles Reyes</a>
+	<br />
+	<a href = "#Elías">2.Elías Janer Madiba</a>
+	<br />
+	<a href = "#Miguel">3.Miguel Mateos Matias</a>
+	<br />
+	<a href = "#Alberto">4.Alberto Dominguez Sierra</a>
+	<br />
+	<a href = "#Carlos">5.Carlos Moreno Pérez</a>
+	<br />
+	<a href = "#Yasser">6.Yasser Takfa Ghazal</a>
+	<br />
+	<!-- Foto y descripcion de los miembros -->
+	<h2>Miembros del grupo</h2>
+	<a id= "Maria"></a>
+	<h3>1.María Magdalena Miralles Reyes</h3>
+	<img src="./img/fotoMaria.jpg" alt="imagenMaria" height="341">
+	<br>
+	<p>Correo:mmiralle@ucm.es</p>
+	<p>En mi tiempo libre me gusta jugar al padel con mis amigos,<br />
+	ir al cine o de compras, ver series en las distintas plataformas digtales,<br />
+	en verano me gusta ir a la playa e ir a chiringuitos para tapear.<br />
+    Como buena aficionada a la playa me gusta reciclar y recoger la basura de las playas.</p>
+	<a id= "Elías"></a>
+	<h3>2.Elías Janer Madiba</h3>
+	<img src="./img/fotoElias.jpg" alt="fotoElias" height="341">
+	<br>
+	<p>Correo:eliasjan@ucm.es</p>
+	<p>Me gusta mucho irme de viaje por Europa con mi familia y amigos<br />
+	ir al gimnasio por las tardes, salir de fiesta los fines de semana.<br />
+	Los domingos por la tarde aprovecho para ir a repartir comida a los pinguinos.
+	</p>
+	<a id= "Miguel"></a>
+	<h3>3.Miguel Mateos Matias</h3>
+	<img src="./img/fotoMiguel.jpg" alt="fotoMiguel" height="341">
+	<br>
+	<p>Correo:migmat05@ucm.es</p>
+	<p>Me gusta ir al gimnasio cuando tengo un rato libre,<br />
+	 los fines de semana salir de fiesta a discotecas con mis amigos,<br />
+	jugar a videojuegos en el pc y bucear.
+	</p>
+	<a id= "Alberto"></a>
+	<h3>4.Alberto Dominguez Sierra</h3>
+	<img src="./img/fotoAlberto.jpg" alt="fotoAlberto" height="341">
+	<br>
+	<p>Correo:domingus@ucm.es</p>
+	<p>Veo los partidos de futbol, y tambien juego con los amigos<br />
+	ver series de Netflix y los fines de semana salir de fiesta con mis amigos.<br />
+    Me gusta mucho nadar y hacer snorkel.
+	</p>
+	<a id= "Carlos"></a>
+	<h3>5.Carlos Moreno Pérez</h3>
+	<img src="./img/fotoCarlos.jpg" alt="fotoCarlos" height="341">
+	<br>
+	<p>Correo:camore12@ucm.es</p>
+	<p>Al ser aficionado del real madrid me gusta mucho ver los partidos<br />
+	de la liga santander, me gusta jugar al futbol y pasar el rato con los amigos<br />
+	ver peliculas y salir con la bici. <br />
+    Soy muy aventurero y me gusta meterme en jaulas para nadar con tiburones.</p>
+	<a id= "Yasser"></a>
+	<h3>6.Yasser Takfa Ghazal</h3>
+	<img src="./img/fotoYasser.jpg" alt="fotoYasser" height="341">
+	<br>
+	<p>Correo:yastakfa@ucm.es</p>
+	<p>Me gusta salir de fiesta con los amigos los fines de semana y a parte de<br />
+		eso soy voluntario en una perrera en la que cuido perretes muy mayores<br />
+		que nadie quiere adoptar. Me gusta salir en barco para ver los delfines.</p>
+	<a href="#principio">Ir al principio de la pagina</a>
+</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,39 @@
+<footer>
+<div id="footer">
+    <div id="menuFooter">
+        <div id="redes-sociales">
+            <div class="ops-v">
+                <a class="ops" href="#" target="_blank">
+                <img src="img/twitter.png" alt="twitter" width = "50" height = "50">
+                </a>
+            </div>
+            <div class="ops-v">
+            <a class="ops" href="#">
+                <img src="img/facebook.png" alt="facebook" width = "50" height = "50">
+                </a>
+            </div>
+
+            <div class="ops-v">
+            <a class="ops" href="#" target="_blank">
+                <img src="img/instagram.png" alt="instagram" width = "50" height = "50">
+                </a>
+            </div>
+
+            <div class="ops-v">
+            <a class="ops" href="#">
+                <img src="img/youtube.png" alt="youtube" target="_blank" width = "50" height = "50">
+                </a>
+            </div>
+        </div>
+
+        <div id="copyright">
+            <p>Copyright 2023©</p>
+            <p>Todos los derechos reservados</p>
+        </div>
+
+        <div id="sobre-nosotros">
+            <p><a href="miembros.php">Sobre nosotros</a></p>    
+        </div>
+    </div>
+</div>
+</footer>
