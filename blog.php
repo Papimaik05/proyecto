482bd98f518cf86cdd81b56152d009044b82282e
camore12 <c14moreno1p02@gmail.com>
2023-03-13 19:37:24 +0100

    vista blog y mantenimiento

---
+++ blog.php
@@ -0,0 +1,28 @@
+<?php
+require_once './includes/config.php';
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Index</title>
+    </head>
+
+<body>
+    
+            <?php
+            require ('cabecera.php');
+            ?>
+	    <main>
+	        <h1>Esta página se encuentra en mantenimiento</h1>
+            <img src="./img/mantenimiento.png"  width ="500"  height ="500"  alt="mantenimiento">
+            <h2>Vuelva pronto!</h2>
+	    </main> 
+        <?php
+            require('pie.php');
+        ?>        
+        
+</body>
+</html>
