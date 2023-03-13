c37f6b25def741fe874eef42ff68fd45a6bcc948
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-03-13 20:12:35 +0100

    Update datos.sql

--- mysql/datos.sql
+++ mysql/datos.sql
@@ -20,3 +20,7 @@ INSERT INTO `nivel` (`numero`, `nombre`, `minimo`, `maximo`) VALUES
 (1, 'delfin', 5, 9),
 (2, 'megalodon', 10, 19),
 (3, 'poseidon', 20, 100);
+
+INSERT INTO `producto` (`nombre`, `id`, `descripcion`, `unidades`, `precio`, `imagen`) VALUES
+('Snorkel Ultrapower', 1, 'Gafas y snorkel de polietileno y titaneo parar resispar sin parar.', 3, 20, './img/snorkel.jpg'),
+('Submarino el coloso', 2, 'Submarino premium delux ideal para disfrutar de nuestras excursiones con el mayor confort imaginado.\r\nSurca los mares descubriendo sus profundidades gracias a la más última tecnolodia de este navío.', 1, 50000, './img/submarino.jpg');
