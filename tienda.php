16be6e0460730f64d56d4e36893aa21a3174081f
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-03-13 20:08:53 +0100

    Update tienda.php

--- tienda.php
+++ tienda.php
@@ -42,17 +42,6 @@ require_once './includes/config.php';
 
 <div class="equipamiento">
 <h2>Equipamiento</h2>
-
-<form action="tienda.php" method="POST">
-<input type="hidden" name="producto" value="AletasSuperBlast">
-<input type="hidden" name="precio" value="20">
-<input type="hidden" name="img" value="./img/alets.jpg">
-<img src="./img/aletas.jpg" width="200" alt = "aletas" height="200">
-<h3>Aletas SuperBlast </h3>
-<p>Aletas de material resistente para nadar a toda velocidad. Se el más molón del mar con las aletas SuperBlast.</p>
-<input type="submit" value="Ver producto" name="botonComprar">
-</form>
-<br><br><br><br>
 <?php
 $productos = Producto::cargarProductos();
 if($productos == false){
@@ -67,16 +56,6 @@ else{
     }
 }
 ?>
-<img src="./img/snorkel.jpg" width="200" alt = "snorkel" height="200">
-<h3>Snorkel UltraPower  &nbsp; <a href="index.php">Comprar</a></h3>
-<p>Gafas y snorkel de polietileno y titaneo parar resispar sin parar.</p>
-<br><br><br><br>
-
-<img src="./img/submarino.jpg" width="200" alt = "submarino" height="200">
-<h3>Submarino el Coloso  &nbsp; <a href="index.php">Comprar</a></h3>
-<p>Submarino premium delux ideal para disfrutar de nuestras excursiones con el mayor confort imaginado.</p>
-<p>Surca los mares descubriendo sus profundidades gracias a la más última tecnolodia de este navío.</p>
-</div>
 
 <?php 
 	if(isset($_REQUEST["botonComprar"])){
