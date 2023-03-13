5ffccddb541ab5dc11f899c51425158ffbadd741
camore12 <c14moreno1p02@gmail.com>
2023-03-13 19:22:08 +0100

    Create datos.sql

---
+++ mysql/datos.sql
@@ -0,0 +1,21 @@
+/*
+  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
+*/
+TRUNCATE TABLE `experiencias`;
+TRUNCATE TABLE `nivel`;
+TRUNCATE TABLE `producto`;
+TRUNCATE TABLE `rol`;
+TRUNCATE TABLE `usuario`;
+
+INSERT INTO `rol` (`numero`, `nombre`) VALUES
+(0, 'admin'),
+(1, 'usuario'),
+(2, 'Gestor de Contenido');
+
+INSERT INTO `usuario` (`username`, `password`, `email`, `rol`, `puntos`) VALUES
+('admin', 'adminpass', 'camore12@ucm.es', 0, 100);
+
+INSERT INTO `nivel` (`numero`, `nombre`, `minimo`, `maximo`) VALUES
+(0, 'cangrejo', 0, 4),
+(1, 'delfin', 5, 9),
+(2, 'megalodon', 10, 19);
