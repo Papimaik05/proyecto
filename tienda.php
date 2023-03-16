a95893cc1edd668360d1547bd0939bcfa003dcb7
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-03-16 10:12:10 +0100

    Update tienda.php

--- tienda.php
+++ tienda.php
@@ -20,7 +20,7 @@ require_once './includes/config.php';
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <link href= "./css/style.css" rel="stylesheet" type="text/css">
+    <link href= "css/style.css" rel="stylesheet" type="text/css">
     <title>Tienda</title>
 </head>
 
