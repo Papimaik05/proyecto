fd2eb433c1094c48bf58bcb0305f161318c49c88
camore12 <c14moreno1p02@gmail.com>
2023-03-28 19:46:19 +0200

    intento d añadir boton de compra

--- vistaExperiencia.php
+++ vistaExperiencia.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/experiencias.php';
 require_once __DIR__.'/includes/level.php';
+require_once './includes/Usuario.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,17 +17,21 @@ require_once __DIR__.'/includes/level.php';
 	require ('./includes/comun/cabecera.php');
 	$id = $_GET["id"];
 	$experiencia = Experiencia::buscaPorId($id);
-
-	echo  "<h1>" . $experiencia->getNombre() . "</h1>";
+	echo "<h1>" . $experiencia->getNombre() . "</h1>";
 	echo '<img src="' . $experiencia->getImagen() . '" width="400" height="400">';
 	echo "<h3>". $experiencia->getDescripcion() ."</h3>";
 	echo "<h3> Nivel minimo requerido: ". ucfirst(level::getNombre($experiencia->getNivelMinimo()))."</h3>";
-	echo  "<h2>" . $experiencia->getPrecio() . " € </h2>"; 	
+	echo "<h2>" . $experiencia->getPrecio() . " € </h2>"; 	
+	echo $usuario=Usuario::buscaUsuario($_SESSION["nombre"]);
+	if (isset($_SESSION["login"]) && ($_SESSION["login"]===true) && (level::getLevel($usuario->getPuntos())>=$experiencia->getNivelMinimo())){ 
+		?>
+		<button onclick='window.location.href="gestorProductos.php"' type='button' > Comprar</button>
+		<?php 
+	}
 	?>
 	<br>
 
     <?php 
-
 	require('./includes/comun/pie.php');
 	?>
 
