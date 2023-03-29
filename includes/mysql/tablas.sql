178b65cac0ff1031052caf11c44248540f9d317d
Papimaik05 <miguelmateosmatias@gmail.com>
2023-03-29 10:41:33 +0200

    tablas

--- includes/mysql/tablas.sql
+++ includes/mysql/tablas.sql
@@ -32,7 +32,29 @@ DROP TABLE IF EXISTS `nivel`;
 DROP TABLE IF EXISTS `producto`;
 DROP TABLE IF EXISTS `usuario`;
 DROP TABLE IF EXISTS `rol`;
+DROP TABLE IF EXISTS `compraexperiencia`;
+DROP TABLE IF EXISTS `compraproducto`;
 
+-- Estructura de tabla para la tabla `compraexperiencia`
+--
+
+CREATE TABLE `compraexperiencia` (
+  `nombre_usuario` varchar(20) NOT NULL,
+  `id_experiencia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compraproducto`
+--
+
+CREATE TABLE `compraproducto` (
+  `nombre_usuario` varchar(20) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `experiencias` (
   `nombre` varchar(25) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -132,10 +154,39 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`username`),
   ADD KEY `rol` (`rol`);
 
+
+-- Indices de la tabla `compraexperiencia`
+--
+ALTER TABLE `compraexperiencia`
+  ADD KEY `nombre_usuario` (`nombre_usuario`),
+  ADD KEY `id_experiencia` (`id_experiencia`);
+
+--
+-- Indices de la tabla `compraproducto`
+--
+ALTER TABLE `compraproducto`
+  ADD KEY `nombre_usuario` (`nombre_usuario`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
 --
 -- Restricciones para tablas volcadas
 --
 
+-- Filtros para la tabla `compraexperiencia`
+--
+ALTER TABLE `compraexperiencia`
+  ADD CONSTRAINT `compraexperiencia_ibfk_1` FOREIGN KEY (`id_experiencia`) REFERENCES `experiencias` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `compraexperiencia_ibfk_2` FOREIGN KEY (`nombre_usuario`) REFERENCES `usuario` (`username`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compraproducto`
+--
+ALTER TABLE `compraproducto`
+  ADD CONSTRAINT `compraproducto_ibfk_1` FOREIGN KEY (`nombre_usuario`) REFERENCES `usuario` (`username`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `compraproducto_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
 --
 -- Filtros para la tabla `experiencias`
 --
@@ -148,6 +199,8 @@ ALTER TABLE `experiencias`
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `rol` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
