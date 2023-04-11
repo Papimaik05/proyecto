9843b6dbb58472e762f7594d673f1b34e1fe8f82
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-04-11 19:29:49 +0200

    style

--- micuenta.php
+++ micuenta.php
@@ -6,7 +6,7 @@ require_once './includes/Usuario.php';
 
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="http://localhost/proyecto/assets/style.css" />
+        <link rel="stylesheet" type="text/css" href="http://localhost/proyecto/assets/style.css?v=3415" />
         <title>Index</title>
     </head>
 
@@ -17,15 +17,24 @@ require_once './includes/Usuario.php';
             
             ?>
 	    <main>
-            Nombre de usuario:
-            <br>
-            <input type="text" name="nombre"  readonly value="<?php echo $_SESSION['nombre'] ?>">
-            <br><br>
-            Email:
-            <br>
-            <input type="email" name="nombre" readonly value="<?php echo $_SESSION['email'] ?>">
-            <br><br>
-            Tienes: 
+            <h1> Datos de sesión </h1>
+            <ul class="lista-datos">
+            <li class="dato">
+                <span class="dato-titulo">Nombre de usuario:</span>
+                <span class="dato-valor"><?php echo $_SESSION['nombre'] ?></span>
+            </li>
+            <li class="dato">
+                <span class="dato-titulo">Correo electrónico:</span>
+                <span class="dato-valor"><?php echo $_SESSION['email'] ?></span>
+            </li>
+            <li class="dato">
+                <span class="dato-titulo">Puntos:</span>
+                <div class="barra">
+                    <div class="nivel" style="width: <?php echo $_SESSION['puntos']?>%;"></div>
+                </div>
+            </li>
+            </ul>
+            Tienes  
             <?php 
             echo $_SESSION['puntos']." puntos"; 
             ?>
@@ -42,9 +51,9 @@ require_once './includes/Usuario.php';
             <fieldset>
                 <legend><b>Actualizar Email</b></legend>
                 Email :<br><input type="email" name="email" > 
+                <br><br><br>
+                <button type="submit" name="emailnuevo">Actualizar email</button>  
             </fieldset>
-            <br>
-            <button type="submit" name="emailnuevo">Actualizar email</button>
             <br><br>
 
 	        <form action="procesarDatos.php" method="post">
@@ -53,10 +62,9 @@ require_once './includes/Usuario.php';
                 Contraseña :<br><input type="password" name="contr" > 
                 <br>
                 Repita Contraseña :<br><input type="password" name="contr2" > 
-        
-            </fieldset>
-            <br>
+            <br><br><br>
             <button type="submit" name="contrnuevo">Actualizar contraseña</button>
+            </fieldset>
             <br><br>
             <fieldset>
             <a href="historialpedidos.php" class="button">Historial de pedidos</a>
