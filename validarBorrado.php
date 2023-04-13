8287d19ef06b7dc4373204b6098ac369ba25cdd7
camore12 <c14moreno1p02@gmail.com>
2023-04-13 19:38:18 +0200

    tabulao

--- validarBorrado.php
+++ validarBorrado.php
@@ -4,26 +4,26 @@
     $mensaje = "";
     $res=true;
     if(isset($_POST['productos'])){
-    $productosSeleccionados = $_POST['productos'];
-    if (!empty($productosSeleccionados)) {
-        foreach ($productosSeleccionados as $id) {
-            if(Producto::borraPorId($id)){
+        $productosSeleccionados = $_POST['productos'];
+        if (!empty($productosSeleccionados)) {
+            foreach ($productosSeleccionados as $id) {
+                if(Producto::borraPorId($id)){
+                }
+                else{
+                    $res = false;
+                }
+            }
+            if($res==false){
+                $mensaje = "No se han podido eliminar todos los productos";
             }
             else{
-                $res = false;
+                $mensaje = "Todos los elementos seleccionados se han borrado con exito";
             }
+        } 
+        else {
+            $mensaje = "Por favor, seleccione al menos un producto.";
         }
-        if($res==false){
-            $mensaje = "No se han podido eliminar todos los productos";
-        }
-        else{
-            $mensaje = "Todos los elementos seleccionados se han borrado con exito";
-        }
-    } 
-    else {
-        $mensaje = "Por favor, seleccione al menos un producto.";
-    }
-    header("Location:borrarProducto.php?mensaje=$mensaje");
+        header("Location:borrarProducto.php?mensaje=$mensaje");
 }else{
     $experienciasSeleccionadas = $_POST['experiencias'];
     if (!empty($experienciasSeleccionadas)) {
