32e016f2c9b36cba3ecd1963b217dc915c6408a4
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-04-28 12:35:37 +0200

    Update tablas.sql

--- includes/mysql/tablas.sql
+++ includes/mysql/tablas.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-03-2023 a las 12:14:14
+-- Tiempo de generación: 28-04-2023 a las 12:34:50
 -- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.0.25
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,100 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `experiencias`
+-- Estructura de tabla para la tabla `comentario`
 --
 
-DROP TABLE IF EXISTS `experiencias`;
-DROP TABLE IF EXISTS `nivel`;
-DROP TABLE IF EXISTS `producto`;
-DROP TABLE IF EXISTS `usuario`;
-DROP TABLE IF EXISTS `rol`;
-DROP TABLE IF EXISTS `compraexperiencia`;
-DROP TABLE IF EXISTS `compraproducto`;
-
-
-
--- --------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `experiencias` (
-  `nombre` varchar(25) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` text NOT NULL,
-  `precio` float NOT NULL,
-  `nivelminimo` int(11) NOT NULL,
-  `puntos` int(11) NOT NULL,
-  `imagen` varchar(50) NOT NULL,
-  PRIMARY KEY(`id`)
+DROP TABLE IF EXISTS `comentario`;
+CREATE TABLE IF NOT EXISTS `comentario` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Usuario` varchar(15) NOT NULL,
+  `Padre` int(11) NOT NULL,
+  `Contenido` text NOT NULL,
+  `Fecha de creación` date NOT NULL,
+  `Me gusta` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Usuario` (`Usuario`),
+  KEY `Padre` (`Padre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `nivel`
---
-
-CREATE TABLE IF NOT EXISTS `nivel` (
-  `numero` int(11) NOT NULL,
-  `nombre` varchar(25) NOT NULL,
-  `minimo` int(11) NOT NULL,
-  `maximo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
-
-CREATE TABLE IF NOT EXISTS `producto` (
-  `nombre` varchar(25) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` text NOT NULL,
-  `unidades` int(11) NOT NULL,
-  `precio` float NOT NULL,
-  `imagen` varchar(50) NOT NULL,
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `noticia` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `imagen` text NOT NULL,
-  `descripción` text NOT NULL,
-  `fecha` date NOT NULL,
-  `titulo` text NOT NULL,
-   PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
---
--- Estructura de tabla para la tabla `rol`
---
-
-CREATE TABLE IF NOT EXISTS `rol` (
-  `numero` int(11) NOT NULL,
-  `nombre` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `username` varchar(20) NOT NULL,
-  `password` varchar(70) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `rol` int(11) NOT NULL,
-  `puntos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 -- Estructura de tabla para la tabla `compraexperiencia`
 --
 
-CREATE TABLE IF NOT EXISTS`compraexperiencia` (
+DROP TABLE IF EXISTS `compraexperiencia`;
+CREATE TABLE IF NOT EXISTS `compraexperiencia` (
   `nombre_usuario` varchar(20) NOT NULL,
-  `id_experiencia` int(11) NOT NULL
+  `id_experiencia` int(11) NOT NULL,
+  KEY `nombre_usuario` (`nombre_usuario`),
+  KEY `id_experiencia` (`id_experiencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -126,77 +60,125 @@ CREATE TABLE IF NOT EXISTS`compraexperiencia` (
 -- Estructura de tabla para la tabla `compraproducto`
 --
 
+DROP TABLE IF EXISTS `compraproducto`;
 CREATE TABLE IF NOT EXISTS `compraproducto` (
   `nombre_usuario` varchar(20) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `unidades` int(11) NOT NULL
+  `unidades` int(11) NOT NULL,
+  KEY `nombre_usuario` (`nombre_usuario`),
+  KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
---
--- Índices para tablas volcadas
---
+
+-- --------------------------------------------------------
 
 --
--- Indices de la tabla `experiencias`
---
-ALTER TABLE `experiencias`
-  ADD KEY `nivelminimo` (`nivelminimo`);
-
---
--- Indices de la tabla `nivel`
---
-ALTER TABLE `nivel`
-  ADD PRIMARY KEY (`numero`);
-
---
--- Indices de la tabla `producto`
+-- Estructura de tabla para la tabla `experiencias`
 --
 
+DROP TABLE IF EXISTS `experiencias`;
+CREATE TABLE IF NOT EXISTS `experiencias` (
+  `nombre` varchar(25) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` text NOT NULL,
+  `precio` float NOT NULL,
+  `nivelminimo` int(11) NOT NULL,
+  `puntos` int(11) NOT NULL,
+  `imagen` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nivelminimo` (`nivelminimo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Indices de la tabla `rol`
+-- Estructura de tabla para la tabla `nivel`
 --
-ALTER TABLE `rol`
-  ADD PRIMARY KEY (`numero`);
+
+DROP TABLE IF EXISTS `nivel`;
+CREATE TABLE IF NOT EXISTS `nivel` (
+  `numero` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `minimo` int(11) NOT NULL,
+  `maximo` int(11) NOT NULL,
+  PRIMARY KEY (`numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Indices de la tabla `usuario`
+-- Estructura de tabla para la tabla `noticia`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`username`),
-  ADD KEY `rol` (`rol`);
 
+DROP TABLE IF EXISTS `noticia`;
+CREATE TABLE IF NOT EXISTS `noticia` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `imagen` text NOT NULL,
+  `descripción` text NOT NULL,
+  `fecha` date NOT NULL,
+  `titulo` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Indices de la tabla `compraexperiencia`
---
-ALTER TABLE `compraexperiencia`
-  ADD KEY `nombre_usuario` (`nombre_usuario`),
-  ADD KEY `id_experiencia` (`id_experiencia`);
-
---
--- Indices de la tabla `compraproducto`
---
-ALTER TABLE `compraproducto`
-  ADD KEY `nombre_usuario` (`nombre_usuario`),
-  ADD KEY `id_producto` (`id_producto`);
+-- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+CREATE TABLE IF NOT EXISTS `producto` (
+  `nombre` varchar(25) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` text NOT NULL,
+  `unidades` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `imagen` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+CREATE TABLE IF NOT EXISTS `rol` (
+  `numero` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  PRIMARY KEY (`numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `rol` int(11) NOT NULL,
+  `puntos` int(11) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `rol` (`rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Restricciones para tablas volcadas
 --
 
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`Usuario`) REFERENCES `usuario` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`Padre`) REFERENCES `comentario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `compraexperiencia`
-
-
---
---
--- Filtros para la tabla `experiencias`
---
-ALTER TABLE `experiencias`
-  ADD CONSTRAINT `experiencias_ibfk_1` FOREIGN KEY (`nivelminimo`) REFERENCES `nivel` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `rol` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
 --
 ALTER TABLE `compraexperiencia`
   ADD CONSTRAINT `compraexperiencia_ibfk_1` FOREIGN KEY (`id_experiencia`) REFERENCES `experiencias` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
@@ -209,11 +191,18 @@ ALTER TABLE `compraproducto`
   ADD CONSTRAINT `compraproducto_ibfk_1` FOREIGN KEY (`nombre_usuario`) REFERENCES `usuario` (`username`) ON UPDATE CASCADE,
   ADD CONSTRAINT `compraproducto_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
-COMMIT;
 --
+-- Filtros para la tabla `experiencias`
+--
+ALTER TABLE `experiencias`
+  ADD CONSTRAINT `experiencias_ibfk_1` FOREIGN KEY (`nivelminimo`) REFERENCES `nivel` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
-
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `rol` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
