3114fedcab836f605e02ced284fea015b57fdbfd
camore12 <c14moreno1p02@gmail.com>
2023-04-28 13:18:13 +0200

    insertar usuario definitivo

--- validarInsercion.php
+++ validarInsercion.php
@@ -2,8 +2,10 @@
     require_once './includes/producto.php';
     require_once './includes/experiencias.php';
     require_once './includes/level.php';
+    require_once './includes/Usuario.php';
     $mensaje = "";
     $ret=false;
+    $error_pass=true;
     if(isset($_POST["nombre"],$_POST["descripcion"],$_POST["unidades"],$_POST["precio"],$_POST["imagen"])&&$_POST["nombre"]!=''&&$_POST["descripcion"]!=''&&$_POST["precio"]!=''&&$_POST["imagen"]!=''&&$_POST["unidades"]!=''){
         $nombre = $_POST["nombre"];
         $descripcion = $_POST['descripcion'];
@@ -11,7 +13,7 @@
         $precio = $_POST['precio'];
         $imagen = $_POST['imagen'];
         $ret = Producto::crea($nombre,$descripcion,$unidades,$precio,$imagen);
-    }elseif(isset($_POST["nombre"],$_POST["descripcion"],$_POST["precio"],$_POST["imagen"],$_POST["puntos"],$_POST["nivelminimo"])&&$_POST["nombre"]!=''&&$_POST["descripcion"]!=''&&$_POST["precio"]!=''&&$_POST["imagen"]!=''&&$_POST["puntos"]!=''){
+    }else if(isset($_POST["nombre"],$_POST["descripcion"],$_POST["precio"],$_POST["imagen"],$_POST["puntos"],$_POST["nivelminimo"])&&$_POST["nombre"]!=''&&$_POST["descripcion"]!=''&&$_POST["precio"]!=''&&$_POST["imagen"]!=''&&$_POST["puntos"]!=''){
         $nombre = $_POST["nombre"];
         $descripcion = $_POST['descripcion'];
         $puntos = $_POST['puntos'];
@@ -19,15 +21,44 @@
         $imagen = $_POST['imagen'];
         $nivelminimo = level::getNumero($_POST['nivelminimo']);
         $ret = Experiencia::crea($nombre,$descripcion,$precio,$nivelminimo,$puntos,$imagen);
+    }else if(isset($_POST["nombre"],$_POST["email"],$_POST["contr"],$_POST["contr2"],$_POST["rol"],$_POST["puntos"])&&$_POST["nombre"]!=''&&$_POST["email"]!=''&&$_POST["contr"]!=''&&$_POST["contr2"]!=''&&$_POST["rol"]!=''&&$_POST["puntos"]!=''){
+        if ( mb_strlen($_POST["contr"]) < 3 ) {
+            $mensaje2 ="ERROR: El password tiene que tener una longitud de al menos 3 caracteres";
+            $error_pass=false;
+        }
+        if($_POST["contr"]!=$_POST["contr2"]){
+            $mensaje3 = "Las contraseñas no coinciden";
+            $error_pass=false; 
+        }
+        if($error_pass){
+            $nombre = $_POST['nombre'];
+            $email = $_POST['email'];
+            $contr = $_POST['contr'];
+            $puntos = $_POST['puntos'];
+            $rol = level::getNumero($_POST['rol']);
+            $ret = Usuario::crea($nombre,$contr,$email,$rol,$puntos);
+            if($ret==false){
+                $mensaje1 = "El usuario ya existe";
+            }
+        }           
     }
     if($ret==true){
         $mensaje =  "Su inserción se ha realizado con éxito";
     }
     else{
-        $mensaje = "Error en la inserción, vuelva a intentarlo";
+        if(isset($_GET['esUs'])){
+            $mensaje0 = "Error en la inserción, vuelva a intentarlo";
+            $mensaje=$mensaje0.'<br>'.$mensaje1.'<br>'.$mensaje2.'<br>'.$mensaje3;
+        }else{
+            $mensaje = "Error en la inserción, vuelva a intentarlo";
+        }
+       
     }
     if(isset($_GET['esExp'])){
         header("Location:insertarExperiencia.php?mensaje=$mensaje");
+    }
+    else if(isset($_GET['esUs'])){
+        header("Location:insertarUsuario.php?mensaje=$mensaje");
     }
     else{
         header("Location:insertarProducto.php?mensaje=$mensaje");
