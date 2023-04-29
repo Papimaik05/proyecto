d0698b48133190657ecbbd56b09879d2fa70467a
elias-jr7 <eliasjan@ucm.es>
2023-04-29 19:20:03 +0200

    mp

--- modificarProducto.php
+++ modificarProducto.php
@@ -37,29 +37,43 @@
                                 foreach($productos as $p){
                                     $id = $p->getId();
                                     $nombre = $p->getNombre();
-                                    echo "<li><input type='radio' name='producto' value='". $id ."
-                                    '> ". $nombre ."</li>";
+                                    $descripcion = $p->getDescripcion();
+                                    $unidades = $p->getUnidades();
+                                    $precio = $p->getPrecio();
+                                    $imagen = $p->getImagen();
+                                    echo "<li><input type='radio' name='producto' value='". $id ."' data-nombre='". $nombre ."' data-descripcion='". $descripcion ."' data-unidades='". $unidades ."' data-precio='". $precio ."' data-imagen='". $imagen ."'> ". $nombre ."</li>";
                                 }
-                        
-                    echo '</ul>';
-                            echo '<label>Ahora selecciona los valores a modificar: </label>';
-                            echo '<fieldset>';
-                            echo '<label for="nombre">Nombre:</label>';
-                            echo '<input type="text" name="nombre" id="nombre"><br><br>';
-                            echo '<label for="descripcion">Descripción:</label>';
-                            echo '<textarea name="descripcion" id="descripcion"></textarea><br><br>';
-                            echo '<label for="unidades">Unidades:</label>';
-                            echo '<input type="number" name="unidades" id="unidades" min="0"><br><br>';
-                            echo '<label for="precio">Precio:</label>';
-                            echo '<input type="number" name="precio" id="precio" min="0"><br><br>';
-                            echo '<label for="imagen">URL de la imagen:</label>';
-                            echo '<input type="text" name="imagen" id="imagen"><br><br>';
-                            echo '</fieldset>';
-                            echo '<input type="submit" value="Enviar">';
-                            echo '</form>';
+                                echo '</ul>';
+                                echo '<label>Ahora selecciona los valores a modificar: </label>';
+                                echo '<fieldset>';
+                                echo '<label for="nombre">Nombre:</label>';
+                                echo '<input type="text" name="nombre" id="nombre"><br><br>';
+                                echo '<label for="descripcion">Descripción:</label>';
+                                echo '<textarea name="descripcion" id="descripcion"></textarea><br><br>';
+                                echo '<label for="unidades">Unidades:</label>';
+                                echo '<input type="number" name="unidades" id="unidades" min="0"><br><br>';
+                                echo '<label for="precio">Precio:</label>';
+                                echo '<input type="number" name="precio" id="precio" min="0"><br><br>';
+                                echo '<label for="imagen">URL de la imagen:</label>';
+                                echo '<input type="text" name="imagen" id="imagen"><br><br>';
+                                echo '</fieldset>';
+                                echo '<input type="submit" value="Enviar">';
+                                echo '</form>';
                             }
                         
                         ?>
+                        <script>
+    var radios = document.querySelectorAll('input[type="radio"][name="producto"]');
+    radios.forEach(function(radio){
+        radio.addEventListener('click', function(){
+            document.getElementById("nombre").value = this.getAttribute("data-nombre");
+            document.getElementById("descripcion").value = this.getAttribute("data-descripcion");
+            document.getElementById("unidades").value = this.getAttribute("data-unidades");
+            document.getElementById("precio").value = this.getAttribute("data-precio");
+            document.getElementById("imagen").value = this.getAttribute("data-imagen");
+        });
+    });
+</script>
                 </div>
             </div>
                    
