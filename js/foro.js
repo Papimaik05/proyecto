64368b7d1c88f381b49f70f55e3ddf74259b7762
yastakfa <100070921+yastakfa@users.noreply.github.com>
2023-05-02 16:50:33 +0200

    Update foro.js

--- js/foro.js
+++ js/foro.js
@@ -2,3 +2,18 @@ function mostrarFormRespuesta(id){
     var formulario = document.getElementById("formRespuesta" + id);
     formulario.style.display = "block";
 }
+function darMeGusta(idComentario) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "actualizarMeGusta.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+        var respuesta = xhr.responseText;
+        var meGusta = parseInt(respuesta);
+        console.log(respuesta);
+        document.getElementById("contador-me-gusta-" + idComentario).innerHTML = meGusta;
+        }
+    };
+    
+    xhr.send("idComentario=" + idComentario);
+}
