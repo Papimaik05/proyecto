4ee149790783f4a9cd31f2865f9522ab41844e96
camore12 <c14moreno1p02@gmail.com>
2023-05-03 19:04:52 +0200

    noticias

--- mostrarnoticias.php
+++ mostrarnoticias.php
@@ -17,26 +17,29 @@ require_once __DIR__.'/includes/noticia.php';
             <main>
                 <div class="container">
                     <h1>Ultimas Noticias</h1>
-                    <div class="noticias">
-            
-            <section>
-            <?php
-            $noticias=Noticia::cargarNoticias();
-            if($noticias==false){
-                echo "No hay experiencias disponibles a la venta";
-            }else{
-            foreach($noticias as $noticia){
-                echo"<h2>".$noticia->getTitulo()."<h2>";
-                echo "<h2>".$noticia->getFecha()."<h2>";
-                echo "<a href='vistaNoticia.php?id=" . urlencode($noticia->getId()) . "'><img src='". $noticia->getImagen() ."'></a>";
-            }
-            }
-            ?>
-        </section>
-        </div>
-
-
-                    
+                    <br><br>
+                    <div class="noticias">                                
+                        <?php
+                        $noticias=Noticia::cargarNoticias();
+                        if($noticias==false){
+                            echo "No hay experiencias disponibles a la venta";
+                        }else{
+                            foreach($noticias as $noticia){
+                                ?>
+                                <div class='noticiaBox'>
+                                <?php
+                                echo "<h2>".$noticia->getTitulo()."<h2>";
+                                echo "<h2>".$noticia->getFecha()."<h2>";
+                                echo "<a href='vistaNoticia.php?id=" . urlencode($noticia->getId()) . "'><img src='". $noticia->getImagen() ."'id='icono_noticia'></a>";
+                                echo "<h3>Clickea en la imagen para ver la noticia</h3>";
+                                echo "<br><br>";
+                                ?>
+                                </div>
+                                <?php
+                            }
+                        }   
+                        ?>                        
+                    </div>   
                 </div>
             </main> 
             <?php
