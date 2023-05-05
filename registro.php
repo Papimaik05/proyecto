385fd6836ea6b68db4ca3ff7c6403190529a52e7
camore12 <c14moreno1p02@gmail.com>
2023-05-05 11:58:09 +0200

    registro

--- registro.php
+++ registro.php
@@ -5,7 +5,7 @@ require_once './includes/config.php';
 
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <link rel="stylesheet" type="text/css" href="./css/style.css?id=3" />
         <title>Registro</title>
     </head>
 
@@ -20,7 +20,6 @@ require ('./includes/comun/cabecera.php');
         <article>  
         <form action="procesarRegistro.php" method="post">
             <fieldset>
-            <legend>Datos del usuario</legend>
                     <div><label>Usuario:</label> <input required type="text" name="nombre" id="campoUser" />
 						<span id="validUser"></span> </div>
 					<div><label>Correo:</label> <input required type="email" name="email" id="campoEmail" />
@@ -33,7 +32,7 @@ require ('./includes/comun/cabecera.php');
                     }
                     ?>
                     <br>
-					<div><button type="submit" name="registro">Registrar</button></div>
+					<div><button type="submit" name="registro">Registrar</button></div>    
             </fieldset>
             <br>
         </form>
