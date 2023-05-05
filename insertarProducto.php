fe4bb4336cc19ffcc5cd090b2b9b7ee7a6312ad6
elias-jr7 <eliasjan@ucm.es>
2023-05-05 12:05:29 +0200

    f

--- insertarProducto.php
+++ insertarProducto.php
@@ -32,7 +32,7 @@ require_once './includes/config.php';
 								<label for="unidades">Unidades:</label>
 								<input type="number" name="unidades" id="unidades" min="0"><br><br>
 								<label for="precio">Precio:</label>
-								<input type="number" name="precio" id="precio" min="0"><br><br>
+								<input type="number"  inputmode="decimal" name="precio" id="precio" step="0.01" min="0"><br><br>
                                 <label for="imagen">Selecciona una imagen:</label>
                                 <input type="file" name="imagen" id="imagen"><br><br>
 								<input type="submit" value="Insertar producto">
